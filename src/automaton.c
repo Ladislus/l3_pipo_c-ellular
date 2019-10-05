677c034fd9d708ce4b3750301820f0aeb522f6d0
Ladislas WALCAK <Ladislas.walcak@etu.univ-orleans.fr>
2019-10-05 16:42:46 +0200

    Added comments in automaton.c and cleaned includes

--- src/automaton.c
+++ src/automaton.c
@@ -1,15 +1,36 @@
-#include <assert.h>
-#include "structure.h"
+/**
+ * @file automaton.c
+ * @author Ladislas WALCAK, Simon DRIEUX
+ * @brief Main file of the executable file
+ * @version 0.1
+ * @date 2019-10-05
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
 #include "menu.h"
 
+/**
+ * @brief Main function
+ * 
+ * @param argc int : (UNUSED) Number of parameters 
+ * @param argv char** : (UNUSED) Array of the parameters
+ * @return int The exit status
+ */
 int main(int argc, char** argv) {
 
+    //Declare a pointer for the automaton
     automaton* a = NULL;
 
+    //Launch the menu and assign an automaton to variable "a"
     a = menu();
 
+    //Start the automaton
     start(a);
+    //Once finished, destroy the automaton
     destroy(&a);
 
+    //Exit the program
     return EXIT_SUCCESS;
 }
